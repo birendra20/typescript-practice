57d5863bc298f7be473ab2b717d259b6b4972ea0
birendra20 <birendragupta20@gmail.com>
2022-10-13 18:05:08 +0530

    array

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,31 @@
+// {
+//   //********type basics */
+//   let char = "abc";
+//   //char=14 we cannot change the variable type it will throw an error
+//   char = "cde"; //works fine
+//   let isBlackBelt = true;
+//   isBlackBelt = false;
+//   // isBlackBelt=23 throws an error
+//   var circ = (diameter: number) => {
+//     //typescript allows type check
+//     return diameter * Math.PI;
+//   };
+//   //console.log(circ("hell")); //throws en error
+//   console.log(circ(7.5));
+// }
 {
-    //********type basics */
-    var char = "abc";
-    //char=14 we cannot change the variable type it will throw an error
-    char = "cde"; //works fine
-    var isBlackBelt = true;
-    isBlackBelt = false;
-    // isBlackBelt=23 throws an error
-    var circ = function (diameter) {
-        return diameter * Math.PI;
-    };
+    //******** arrays***** */
+    var names = ["alpha", "bravo", "charlie"];
+    names.push("toad");
+    //names.push(3) //error
+    //names[0] = 3; //err
+    var numbers = [10, 20, 30, 40];
+    numbers.push(25);
+    //numbers.push("shaun")
+    //numbers[1] = "shaun"
+    var mixed = ["ken", 4, "abc", 8, 9];
+    mixed.push("ryu");
+    mixed.push(10);
+    mixed[0] = 3;
+    //mixed[1] = true; //err boolean value does not exist while declaring
 }
-//console.log(circ("hell")); //throws en error
-console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,40 @@
+// {
+//   //********type basics */
+
+//   let char = "abc";
+
+//   //char=14 we cannot change the variable type it will throw an error
+//   char = "cde"; //works fine
+
+//   let isBlackBelt = true;
+//   isBlackBelt = false;
+//   // isBlackBelt=23 throws an error
+
+//   var circ = (diameter: number) => {
+//     //typescript allows type check
+//     return diameter * Math.PI;
+//   };
+//   //console.log(circ("hell")); //throws en error
+//   console.log(circ(7.5));
+// }
 {
-  //********type basics */
+  //******** arrays***** */
 
-  let char = "abc";
+  let names = ["alpha", "bravo", "charlie"];
 
-  //char=14 we cannot change the variable type it will throw an error
-  char = "cde"; //works fine
+  names.push("toad");
+  //names.push(3) //error
+  //names[0] = 3; //err
 
-  let isBlackBelt = true;
-  isBlackBelt = false;
-  // isBlackBelt=23 throws an error
+  let numbers = [10, 20, 30, 40];
 
-  var circ = (diameter: number) => {
-    return diameter * Math.PI;
-  };
+  numbers.push(25);
+  //numbers.push("shaun")
+  //numbers[1] = "shaun"
+
+  let mixed = ["ken", 4, "abc", 8, 9];
+  mixed.push("ryu");
+  mixed.push(10);
+  mixed[0] = 3;
+  //mixed[1] = true; //err boolean value does not exist while declaring
 }
-//console.log(circ("hell")); //throws en error
-console.log(circ(7.5));
