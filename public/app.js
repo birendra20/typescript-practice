1475c59a2502afcd8940461391082dba92db3f49
birendra20 <birendragupta20@gmail.com>
2022-10-14 17:07:05 +0530

    module

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-"use strict";
+import { Invoice } from "./classes/Invoice.js";
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("max", "work on the max website", 350);
+const invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invOne, invTwo);
+const form = document.querySelector(".new-item-form");
+//inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import { Invoice } from "./classes/Invoice.js";
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("max", "work on the max website", 350);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invOne, invTwo);
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
