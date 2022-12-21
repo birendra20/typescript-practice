c8f7eb312f42fca3d60fa084992840ef0e012e29
birendra20 <birendragupta20@gmail.com>
2022-12-21 20:21:29 +0530

    function

---
+++ free-codecamp-tutorials/02_basics/myfunctions.js
@@ -0,0 +1,30 @@
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var myValue = addTwo(5);
+getUpper("hitesh");
+signUpUser("hitesh", "hitesh@lco.dev", false);
+loginUser("h", "h@h.com");
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+// The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+// never also appears when TypeScript determines there’s nothing left in a union.
