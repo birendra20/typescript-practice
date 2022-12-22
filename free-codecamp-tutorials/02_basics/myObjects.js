40c995e3e8992cd4af181cdcaa1480ba33ed731e
birendra20 <birendragupta20@gmail.com>
2022-12-22 15:09:40 +0530

    objects

---
+++ free-codecamp-tutorials/02_basics/myObjects.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var User = {
+    name: "hitesh",
+    email: "hitesh@lco.dev",
+    isAvtive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+var newUser = { name: "hitesh", isPaid: false, email: "h@h.com" };
+createUser(newUser);
